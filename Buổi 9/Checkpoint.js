fb0a9140d88c2b698edd629592aab8709dcada86
NamDepTrai0201 <27KQrtd.thn@gmail.com>
2022-08-14 16:42:21 +0700

    Checkpoint

--- Buổi 9/Checkpoint.js
+++ Buổi 9/Checkpoint.js
@@ -1,13 +1,26 @@
-let GmailOrUsernameInput = []
-const addBoth = ()=>{
-    let GmailOrUsername ={
-        GmailOrUsername: document.getElementById('GmailOrUsername').value,
-        Passwords: document.getElementById('Passwords').value
-    };
-    GmailOrUsernameInput.push(GmailOrUsername);
-    document.forms[0].reset();
-    console.log(GmailOrUsername);
-    
-    localStorage.setItem("Thông tin người dùng" , JSON.stringify(GmailOrUsernameInput))
-};
-    document.getElementById('ButtonSignIn').addEventListener('click' , addBoth);
+// Lưu trữ thông tin vào local
+
+// let GmailOrUsernameInput = []
+// const addBoth = ()=>{
+//     let GmailOrUsername ={
+//         GmailOrUsername: document.getElementById('GmailOrUsername').value,
+//         Passwords: document.getElementById('Passwords').value
+//     };
+//     GmailOrUsernameInput.push(GmailOrUsername);
+//     document.forms[0].reset();
+//     console.log(GmailOrUsername);
+
+//     localStorage.setItem("Thông tin người dùng" , JSON.stringify(GmailOrUsernameInput))
+// };
+//     document.getElementById('ButtonSignIn').addEventListener('click' , addBoth);
+//------------------------------------------------------------------------------------------
+// Đăng nhập và thông báo
+ButtonSignIn.onclick = function(){
+    var GmailOrUserName = document.getElementById("GmailOrUsername").value;
+    var Password = document.getElementById("Passwords").value;
+    if(GmailOrUserName==="27KQrtd.thn@gmail.com" && Password==="ohlord"){
+        alert("login succesfully")
+    }else{
+        alert("login failed")
+    }
+}
