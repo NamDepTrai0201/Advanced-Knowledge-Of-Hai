213c0aacd435b83e7d0d05f07c672fab756d72d9
NamDepTrai0201 <27KQrtd.thn@gmail.com>
2022-09-22 14:19:10 +0700

    Update

--- Dự án cuối khóa gẹt gô/Dack.js
+++ Dự án cuối khóa gẹt gô/Dack.js
@@ -6,6 +6,20 @@ function login(){
     }else{
         alert("Log in failed")
     }
-}
+};
+// let giohang = document.querySelectorAll('.AddToCartButton2')
 
-
+// for(let i = 0 ; i < giohang.length ; i++){
+//     giohang[i].addEventListener("click" , ()=>{
+//         Soluonggiohang()
+//     })
+// };
+// function Soluonggiohang(){
+//     let soluong = localStorage.getItem('Soluonggiohang')
+//     soluong = parseInt(soluong)
+//     if ( soluong ){
+//         localStorage.setItem('Soluonggiohang' , soluong + 1);
+//     }else{
+//         localStorage.setItem('Soluonggiohang' , 1);
+//     }
+// }
